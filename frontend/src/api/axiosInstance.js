0d8b8ca487733df8f9aa5a231aa9f2a7f29b2796
aaryaa-infobeans <aaryaa.bhosale@infobeans.com>
2025-11-18 21:24:17 +0530

    Update DNS for AWS EC2 instance

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api/v1';
 
-const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://ec2-34-228-74-203.compute-1.amazonaws.com:8000/api/v1';
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://ec2-3-88-107-2.compute-1.amazonaws.com:8000/api/v1';
 
 const axiosInstance = axios.create({
   baseURL,
